0960295e417cfabece6971f4d2013e1610e2606e
skalwar00 <skalwar00@gmail.com>
2024-08-25 20:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def download_file(filename):
     return send_file(os.path.join("processed", filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=5000)
